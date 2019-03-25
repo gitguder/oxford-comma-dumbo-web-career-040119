ee2859973396509f2968df108535976bd56f52c6
Codey Pinero <codey.pinero@gmail.com>
2019-03-25 16:36:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   if array.size == 2
     return "#{array.first} and #{array.last}"
   elsif array.size > 2
-    array[-1].insert(0, " and ")
+    array[-1].insert(0, "and ")
   end
   array.join(", ")
 end
