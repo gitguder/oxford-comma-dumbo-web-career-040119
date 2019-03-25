ba466188a45d87d40cd06699292e1e5b466f1dec
Codey Pinero <codey.pinero@gmail.com>
2019-03-25 16:36:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,16 @@
 def oxford_comma(array)
   array.join
-  #if array.length == 2 #if array.size == 2
-    #return "#{array[0]} and #{array[1]}" #return "#{array.first} and #{array.last}"
-  #elsif 2 < array.length #elsif array.size > 2
-    #array[-1].insert(0, "and ") #array[-1].insert(0, " and ")
-  #end
-  #array.join(", ")
+  if array.size == 2
+    return "#{array.first} and #{array.last}"
+  elsif array.size > 2
+    array[-1].insert(0, " and ")
+  end
+  array.join(", ")
 end
+
+#if array.length == 2 #if array.size == 2
+  #return "#{array[0]} and #{array[1]}" #return "#{array.first} and #{array.last}"
+#elsif 2 < array.length #elsif array.size > 2
+  #array[-1].insert(0, "and ") #array[-1].insert(0, " and ")
+#end
+#array.join(", ")
