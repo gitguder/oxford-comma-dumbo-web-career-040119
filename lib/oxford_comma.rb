33a48c08048a09f499a28185d23d0f889279fb29
Codey Pinero <codey.pinero@gmail.com>
2019-03-25 16:37:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,9 +8,3 @@ def oxford_comma(array)
   array.join(", ")
 end
 
-#if array.length == 2 #if array.size == 2
-  #return "#{array[0]} and #{array[1]}" #return "#{array.first} and #{array.last}"
-#elsif 2 < array.length #elsif array.size > 2
-  #array[-1].insert(0, "and ") #array[-1].insert(0, " and ")
-#end
-#array.join(", ")
